682b5c3674003c439f201e1bea8c626f6c832902
temur_p <temurpallaev@gmail.com>
2016-06-09 14:17:57 +0500

    updated about page, modified url.py (now compatible with django 1.10)

--- djangotest1/urls.py
+++ djangotest1/urls.py
@@ -15,12 +15,13 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from blog import views
 
 urlpatterns = [
-	url(r'^$', 'blog.views.post_list', name='post_list'),
-	url(r'^post/(?P<pk>\d+)/$', 'blog.views.post_detail', name = 'post_detail'),
-	url(r'^about/$', 'blog.views.about', name='about'),
-	url(r'^post/new/$', 'blog.views.post_new', name='post_new'),
-	url(r'^post/(?P<pk>\d+)/edit/$', 'blog.views.post_edit', name='post_edit'),
+	url(r'^$', views.post_list, name='post_list'),
+	url(r'^post/(?P<pk>\d+)/$', views.post_detail, name = 'post_detail'),
+	url(r'^about/$', views.about, name='about'),
+	url(r'^post/new/$', views.post_new, name='post_new'),
+	url(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
     url(r'^admin/', admin.site.urls, name='admin_panel'),
 ]
